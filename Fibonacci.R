142cb227c6a3280514a291e172c38b6cd643e45c
Ryan Daniels <jryan.daniels@gmail.com>
2023-11-28 15:05:21 +0100

    making meaningless edits

--- Fibonacci.R
+++ Fibonacci.R
@@ -10,3 +10,13 @@ fibonacci <- function(n) {
 n <- 10
 result <- fibonacci(n)
 cat("The", n, "th Fibonacci number is:", result)
+
+# Example usage for another number
+n <- 11
+# expecting the answer to be 89
+result <- fibonacci(n)
+# changing the print function
+print(c("Another test, this time with the ", n, "th Fibonacci number is:", result))
+
+
+
